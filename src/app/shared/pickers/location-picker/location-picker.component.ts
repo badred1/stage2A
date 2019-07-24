0bf9f2e1ebf4cb1b5e06ac1e488352b7be126d5b
Anas02200 <rafa0anas@gmail.com>
2019-07-25 00:47:41 +0100

    maps v1

---
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -0,0 +1,25 @@
+import { MapModalComponent } from "./../../map-modal/map-modal.component";
+import { Component, OnInit } from "@angular/core";
+import { ModalController } from "@ionic/angular";
+
+@Component({
+  selector: "app-location-picker",
+  templateUrl: "./location-picker.component.html",
+  styleUrls: ["./location-picker.component.scss"]
+})
+export class LocationPickerComponent implements OnInit {
+  constructor(private modalCtrl: ModalController) {}
+
+  ngOnInit() {}
+
+  onPickLocation() {
+    this.modalCtrl.create({ component: MapModalComponent }).then(modalEl => {
+      modalEl.onDidDismiss().then(modalData => {
+        console.log(modalData.data)
+    
+      });
+      modalEl.present();
+    });
+}
+
+}
